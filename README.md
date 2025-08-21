faccf9475c64c141f09ff0ba3dceea39abd899b6
liam.guruge <liam.guruge@roivant.com>
2025-08-21 10:58:50 -0400

    add template

---
+++ README.md
@@ -0,0 +1,12 @@
+## Roivant Salt Formula Template
+
+Fork of:
+
+https://github.com/saltstack-formulas/template-formula.git
+
+Upstream set:
+
+Modified kitchen file to only include OS's we want.
+
+
+also see README.rst in docs folder.
---
+++ release.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // TODO: remove this when we no longer process releases on GitLab CI
+  repositoryUrl: 'https://github.com/saltstack-formulas/template-formula',
+  plugins: [
+    ['@semantic-release/commit-analyzer', {
+      preset: 'angular',
+      releaseRules: './release-rules.js'
+    }],
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', {
+      changelogFile: 'CHANGELOG.md',
+      changelogTitle: '# Changelog'
+    }],
+    ['@semantic-release/exec', {
+      // eslint-disable-next-line no-template-curly-in-string
+      prepareCmd: 'sh ./pre-commit_semantic-release.sh ${nextRelease.version}'
+    }],
+    ['@semantic-release/git', {
+      assets: ['*.md', 'docs/*.rst', 'FORMULA']
+    }],
+    '@semantic-release/github'
+  ],
+  generateNotes: {
+    preset: 'angular'
+  }
+}
